b337f023b7361c3fe54f3ffce9f359a52e33a292
Vipul Tyagi <vipul.tyagi01@ad.infosys.com>
2021-11-18 18:39:13 +0530

    Added Routes and mapped components to the routes

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Home extends React.Component{
     render(){
         return(
-            <h1 style={{color: 'red', textAlign: 'center'}}>I am at Home</h1>
+            <h1 style={{color: 'red',textAlign: 'center'}}>I am at Home</h1>
         );
     }
 }
