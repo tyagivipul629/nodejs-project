622761781b847f203efbad60ee5a4275039cf18d
abhishek <abhishek.kumar13103@infoys.com>
2021-11-19 18:47:34 +0530

    Final  signup page

--- src/components/Signup/signup.js
+++ src/components/Signup/signup.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
+import axios from 'axios'
 
 class signup extends React.Component{
     constructor(){
@@ -16,35 +17,111 @@ class signup extends React.Component{
         }
     }
     
+    handlecall=(state)=>{
+        console.log("method called")
+        console.log(state);
+        axios.post('http://localhost:5000/signup',state).then((res)=>{
+            console.log('added');
+                
+
+        }).catch(()=>{
+         console.log('some err in signup of products');
+        })
+    }
+
+
 
     handleChange = (e) => {
         this.setState({
             [e.target.name]:e.target.value,
         message:''})
     }
-    handleSubmit = (e) => {
+    handleSubmit = (e,state) => {
         e.preventDefault();
         const validateEmail = (email) => {
             var re = /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i;
             return re.test(email);
         }
+
+        const validatePass=(str)=>{
+            console.log(str)
+            var upper = 0,
+            lower = 0,
+            number = 0,
+            special = 0;
+          for (var i = 0; i < str.length; i++)
+          {
+            if (str[i] >= "A" && str[i] <= "Z") upper++;
+            else if (str[i] >= "a" && str[i] <= "z") lower++;
+            else if (str[i] >= "0" && str[i] <= "9") number++;
+            else special++;
+          }
+          console.log(lower,upper,special)
+          if(upper>0 && lower>0 && number>0 && special>0){return true}
+          else{return false}
+        }
+
+        const validateUname=(str)=>{
+            var upper = 0,
+            lower = 0,
+            number = 0,
+            special = 0;
+            for(var i=0;i<str.length;++i){
+                if (str[i] >= "A" && str[i] <= "Z") upper++;
+                else if (str[i] >= "a" && str[i] <= "z") lower++;
+                else if (str[i] >= "0" && str[i] <= "9") number++;
+                else special++;
+            }
+        
+            if(special>0 || number>0) {return false}
+            else{return true}
+          
+        }
+        const confirmPass=(pass1,pass2)=>{
+            if(pass1!==pass2) return false
+            else return true
+        }
+        function success(){
+            setTimeout(function(){
+             console.log("signup")
+            },5000)
+        }
+        
         if(this.state.email =='' || this.state.password1=='' || this.state.password2=='' || this.state.uname=='' || this.state.phone==''){
             this.setState({message:"All fields are mandatory", textStyle:"danger"})
         } 
-        else {
-            if(validateEmail(this.state.email) && validatePass(this.state.password1) && validateUname(this.state.uname)){
-                this.setState({message:"Looks good!",
-                textStyle:"success"
-                })
-            }
-            else {
-                this.setState({message:"Please enter a valid email id",
-                textStyle:"danger"
-            })
+       else{
+           if(validateEmail(this.state.email)){
+               if(validatePass(this.state.password1)){
+                   if(validateUname(this.state.uname)){
+                       if(confirmPass(this.state.password1,this.state.password2)){
+                        this.setState({message:"Succesfully Signed Up"})
+                          success()
+                          this.props.history.replace('/home');
+                       }else{
+                        this.setState({message:"Passwords are not matching"})
+                       }
+                     
+                   }
+                   else{
+                    this.setState({message:"Username is not valid(should contain characters only)"})
+                   }
+                }
+                else{
+                    this.setState({message:"Password is not valid (Atleast 1 digit,upperCase ,LowerCase && special character needed)"})
+                } 
+            } else{
+                this.setState({message:"Email is not valid(Enter proper email id)"})
+            }  
         }
+        
+           
+       
+        
+       
       
-    } 
-    this.props.history.replace('/login');  
+    
+    
 }
     render(){
         return(
@@ -53,30 +130,30 @@ class signup extends React.Component{
                
    
     
-    <form  style={{position:'relative',left:'50px'}} onSubmit={this.handleSubmit} >
+    <form  style={{position:'relative',left:'50px'}} onSubmit={this.handleSubmit} onClick={()=>{this.handlecall(this.state)}}>
                   <b><h5>SignUp</h5></b><br/>
                     <div className="form-group">
                         <label>Username:</label>
-                        <input style={{width:'40%'}} placeholder="Enter your username" className="form-control" name="uname" type="text" onChange={this.handleChange}/>
+                        <input style={{width:'40%'}} placeholder="Only Characters Allowed(First Name)" className="form-control" name="uname" type="text" onChange={this.handleChange}/>
                     </div>
                     <div className="form-group">
                         <label>Email Address:</label>
-                        <input style={{width:'40%'}} placeholder="Enter your email" className="form-control" name="email" type="text" onChange={this.handleChange}/>
+                        <input style={{width:'40%'}} placeholder="ex:abc@gmail.com" className="form-control" name="email" type="text" onChange={this.handleChange}/>
                     </div>
                     <div className="form-group">
                         <label>Phone Number:</label>
-                        <input style={{width:'40%'}}  placeholder="Enter your phone-number"className="form-control" name="phone" type="number" onChange={this.handleChange} maxLength="10"/>
+                        <input style={{width:'40%'}}  placeholder="ex:0123456789"className="form-control" name="phone" type="number" onChange={this.handleChange} maxLength="10"/>
                     </div>
                     <div className="form-group">
                         <label>Password:</label>
-                        <input style={{width:'40%'}}  placeholder="Enter the password"className="form-control" name="password1" type="password" onChange={this.handleChange}/>
+                        <input style={{width:'40%'}}  placeholder="Aa@1"className="form-control" name="password1" type="password" onChange={this.handleChange}/>
                     </div>
                     <div className="form-group">
                         <label>Confirm Password:</label>
-                        <input style={{width:'40%'}}  placeholder="Eonfirm the password"className="form-control" name="password2" type="password" onChange={this.handleChange}/>
+                        <input style={{width:'40%'}}  placeholder="Aa@1"className="form-control" name="password2" type="password" onChange={this.handleChange}/>
                     </div>
                     {this.state.message !== '' && <div className={`text text-${this.state.textStyle}`}>{this.state.message}</div>}<br/>
-                    <submit className="btn btn-primary" onClick={this.handleSubmit}>Register</submit>
+                    <submit className="btn btn-success" onClick={this.handleSubmit}>Register</submit>
                 </form>
                
     
