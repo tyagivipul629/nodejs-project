f851b85250fb7fa96b0779a36ad506b5e072a759
Vignesh Kumar R <vigneshkumar.r02@infosys.com>
2021-11-19 13:28:08 +0530

    Test

--- src/components/Orders/orders.js
+++ src/components/Orders/orders.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Home extends React.Component {
     render() {
         return (
-            <h1>My Orders</h1>
+            <div className="container mt-5">
+                <h1>My Orders</h1>
+            </div>
         );
     }
 }
