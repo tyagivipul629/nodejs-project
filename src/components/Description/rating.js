8019fc31822ced4f766c583995218dc686ed89b5
Vindhya Chalamolu(vindhya.chalamolu) <vindhya.chalamolu@infosys.com>
2021-11-22 10:01:26 +0530

    Product Description

--- src/components/Description/rating.js
+++ src/components/Description/rating.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './rate.css'
 class Rater extends React.Component {
   render() {
-      console.log("rating")
+     
     let items = [];
     for (var i = 1; i < this.props.maxlength; i++) {
-        console.log("gdgdas")
+    
       let clickHandler = this.props.onSelected && this.props.onSelected.bind(null, i);
       items.push(<li key={i} className={i <= this.props.value ? 'filled' : ''} onClick={clickHandler}>{'\u2605'}</li>)
     }
     return (
-      <ul className="rating">hello{items}</ul>
+      <ul className="rating">{items}</ul>
     )
   }
 }
